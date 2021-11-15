35faa16cd3b9e5a0b7625b625166d6281dd17425
kerem3338 <73388608+kerem3338@users.noreply.github.com>
2021-11-15 14:20:35 +0300

    2.2
    2.2

--- animator.py
+++ animator.py
@@ -14,15 +14,30 @@ app.play()
 import os
 import sys
 import time
-from playsound import playsound
-#["foo", "bar", "baz"].index("bar")
+import locale
+
+
+"""
+class Sound:
+	def __init__(self):
+		self.p = None
+	def play_sound(self, soundfile):
+		self.p = multiprocessing.Process(target=playsound, args=(soundfile,))	
+		self.p.start()
+	def stop(self):
+		self.p.terminate()
+"""
+
+
 class Animator:
     def __init__(self, sleep=1.0):
         self.scenes_count = 0
         self.scenes = []
+        #self.sound = Sound()
         self.sleep = sleep
-        self.version = "2.1"
-        
+        self.version = "2.2"
+        self.system_lang = locale.getdefaultlocale()[0]
+
     def version(self):
         return self.version
 
@@ -34,12 +49,12 @@ class Animator:
         with open(file, "r", encoding=encoding) as file:
             self.scenes.append(file.read())
             self.scene_count += 1
-            
+
     def scene(self, scene):
         """add new scene"""
         self.scenes.append(scene)
         self.scenes_count += 1
-        
+
     def shape(self, shape, position=None):
         """return shape"""
         square = "##\n##"
@@ -47,19 +62,25 @@ class Animator:
             return square
         else:
             print(f"InvalidShape: {shape}")
-            
+    """
+    def play_sound(self,file):
+        self.sound.play_sound(file)
+    def stop_sound(self):
+        self.sound.stop()
+    """
+
     def copy_last(self, copy_count=None):
         """copy last scene"""
         if copy_count is None:
             last_scene = len(self.scenes)-1
             self.scenes.append(self.scenes[last_scene])
-            self.scenes_count +=1
+            self.scenes_count += 1
         else:
             for i in range(copy_count):
                 self.scenes_count += 1
                 last_scene = len(self.scenes)-1
                 self.scenes.append(self.scenes[last_scene])
-                
+
     def copy_from_id(self, id, copy_count=None):
         if copy_count is None:
             scene = self.scenes[id]
@@ -69,38 +90,29 @@ class Animator:
             for i in range(copy_count):
                 self.scenes_count += 1
                 self.scenes.append(self.scenes[id])
-        
-    
-    def play_sound(self, soundfile, background:bool=True):
-        if background is True:
-            playsound(soundfile, False)
-        elif background is False:
-            playsound(soundfile)
-        else:
-            print(f"InvalidArgument: {backgound}")
-            
+
     def scene_from_id(self, id):
         print(self.scenes[id])
-        
+
     def list_scenes(self):
         for i in range(len(self.scenes)):
             print(self.scenes[i])
-            
+
     def scenes_count(self):
-        """NOT:Sahne içinde kullanılırsa bulunduğu sahneyi eklemez örnek: eğer projenizde 6 sahne varsa 5 sahne gösterecektir eğer sahnenin içinde kullanmazsanız sahne sayınızı normal bir şekilde gösterecektir""" 
+        """NOT:Sahne içinde kullanılırsa bulunduğu sahneyi eklemez örnek: eğer projenizde 6 sahne varsa 5 sahne gösterecektir eğer sahnenin içinde kullanmazsanız sahne sayınızı normal bir şekilde gösterecektir"""
         return self.scenes_count
-    
-    def set_sleep(self,sleep):
+
+    def set_sleep(self, sleep):
         self.sleep = sleep
-    
+
     def clear(self):
         """clear screen"""
         if os.name == "nt":
-            
+
             os.system("cls")
         else:
             os.system("clear")
-            
+
     def play(self):
         """Play all scenes"""
         for i in range(len(self.scenes)):
@@ -109,10 +121,41 @@ class Animator:
             time.sleep(self.sleep)
             self.clear()
 
+    def export_scenes_dir(self, dir, fileextension="txt"):
+        if self.system_lang == "tr_TR":
+            print("Yapmak istediğiniz işlem Kritik bir işlemdir\nEğer sahne sayısı fazla bir animasyonun çıktısını almak istiyorsanız bu yöntem öenerilmez\nBilgisayarınızın hızını düşürebilir ayrıca diskte baya yer kaplar")
+            onayla = input("Onaylıyormusunuz (evet/hayır/hayir)")
+            if onayla == "evet":
+                pass
+            elif onayla == "hayır" or "hayir":
+                sys.exit()
+        else:
+            print("The operation you want to do is a critical operation\nIf you want to output an animation with a large number of scenes, this method is not recommended\nIt may reduce the speed of your computer, and it also takes up a lot of space on the disk.")
+            onayla = input("Do you confirm (yes/no)")
+            if onayla == "yes":
+                pass
+            elif onayla == "no":
+                sys.exit()
+
+        if os.path.isdir(dir):
+            for i in range(len(self.scenes)):
+                with open(f"{dir}\\{i}.{fileextension}", "w") as file:
+                    file.write(self.scenes[i])
+        else:
+            if self.system_lang == "tr_TR":
+                print(f"{dir} Dosya yolu bulunamadı")
+            else:
+                print("f{dir} File path not found")
+
     def export_scenes(self, exportfile, encoding="utf8"):
         """export all scenes"""
         with open(exportfile, "w", encoding=encoding) as exportfile:
-            exportfile.write(f"Created using Boip Animator {self.version}")
+
+            if self.system_lang == "tr_TR":
+                exportfile.write(
+                    f"Boip Animator Kullanılarak yapıldı {self.version}")
+            else:
+                exportfile.write(f"Created using Boip Animator {self.version}")
             for i in range(len(self.scenes)):
                 exportfile.write("\n")
                 exportfile.write(self.scenes[i])
